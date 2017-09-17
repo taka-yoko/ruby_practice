5bfb2fe9b13bccf1ef244a8606f687a0e046c816
yokotaka <tamoriinu@gmail.com>
2017-09-17 14:45:46 +0000

    define Enumerable#take_by

--- topword.rb
+++ topword.rb
@@ -1,4 +1,4 @@
-class Hash
+module Enumerable
   def take_by(nth)
     sort_by { |elem| yield elem }.take(nth)
   end
@@ -7,4 +7,4 @@ end
 WORDS = ARGF.read.downcase.scan(/[a-z]+/)
 DICTIONARY = WORDS.reduce(Hash.new(0)) { |dic, word| dic[word] += 1; dic }
 p DICTIONARY.take_by(30) { |key, val| -val }
-p DICTIONARY.take_by(30) { |key, val| val }
+p WORDS.take_by(30) { |word| -word.length }
