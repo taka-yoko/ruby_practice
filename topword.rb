4678cba68acde1daebcff36d33490d7c6f262b55
yokotaka <tamoriinu@gmail.com>
2017-09-17 13:27:54 +0000

    sort_by method

--- topword.rb
+++ topword.rb
@@ -1,3 +1,3 @@
 WORDS = ARGF.read.downcase.scan(/[a-z]+/)
 DICTIONARY = WORDS.reduce(Hash.new(0)) { |dic, word| dic[word] += 1; dic }
-p DICTIONARY.sort { |a, b| b[1] <=> a[1] }[0...30]
+p DICTIONARY.sort_by { |key, val| val }.slice(-30..-1).reverse
