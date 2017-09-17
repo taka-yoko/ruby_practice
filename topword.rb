73e954596a2fd4fb15927e9b257da8cfa4fdb754
yokotaka <tamoriinu@gmail.com>
2017-09-17 15:00:39 +0000

    refine top_by_value method

--- topword.rb
+++ topword.rb
@@ -6,15 +6,15 @@ end
 
 class Hash
   def top_by_value(nth)
-    take_by(nth) { |key, val| -val }
+    select { |key, val| yield val }.take_by(nth) { |key, val| -val }
   end
   
   def bottom_by_value(nth)
-    take_by(nth) { |key, val| val }
+    select { |key, val| yield val }.take_by(nth) { |key, val| val }
   end
 end
 
 WORDS = ARGF.read.downcase.scan(/[a-z]+/)
 DICTIONARY = WORDS.reduce(Hash.new(0)) { |dic, word| dic[word] += 1; dic }
-p DICTIONARY.top_by_value(30)
-p DICTIONARY.bottom_by_value(30)
+p DICTIONARY.top_by_value(30){ |val| val < 400}
+p DICTIONARY.bottom_by_value(30) { |val| val > 400 }
