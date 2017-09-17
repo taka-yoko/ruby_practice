07f17ed899f65a3574826f59460f21ec2894c130
yokotaka <tamoriinu@gmail.com>
2017-09-17 13:30:25 +0000

    take method

--- topword.rb
+++ topword.rb
@@ -1,3 +1,3 @@
 WORDS = ARGF.read.downcase.scan(/[a-z]+/)
 DICTIONARY = WORDS.reduce(Hash.new(0)) { |dic, word| dic[word] += 1; dic }
-p DICTIONARY.sort_by { |key, val| val }.slice(-30..-1).reverse
+p DICTIONARY.sort_by { |key, val| -val }.take(30)
